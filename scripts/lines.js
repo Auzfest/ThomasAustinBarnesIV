d53c79e61184f595bccf0e7a9928d38569eb7182
Auzfest <114009523+Auzfest@users.noreply.github.com>
2024-01-10 18:30:18 -0700

    Moving Background made

--- scripts/lines.js
+++
@@ -1,9 +0,0 @@
-const lines = document.createElement("div");
-lines.className = "lines";
-document.body.appendChild(lines);
-
-let scrollPos = 0;
-window.addEventListener("scroll", function () {
-  scrollPos = window.scrollY;
-  lines.style.backgroundPositionX = `${scrollPos}px`;
-});
